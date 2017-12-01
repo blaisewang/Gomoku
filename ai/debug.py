5a2437f4dbcaebccc486a93bfd602fae8f2d7a02
blaisewang <kaito.kaito.k@gmail.com>
2017-12-01 02:40:15 +0000

    pattern matching code refactoring

--- ai/debug.py
+++ ai/debug.py
@@ -1,3 +1,5 @@
+import numpy
+
 import ai
 import ai.evaluate
 
@@ -12,9 +14,9 @@ def pattern_match_debug(y, x):
         ([0, player, player, player, player, 0], 4),
     ]
 
-    one_dimensional_important_pattern_list = [
-        ([0, opponent, opponent, opponent, player], 4, 1),
-        ([player, opponent, opponent, opponent, 0], 0, 5)
+    one_dimensional_dangerous_pattern_list = [
+        ([0, opponent, opponent, opponent, player], True, 4, 1),
+        ([opponent, opponent, opponent, opponent, player], True, 4, 1)
     ]
 
     two_dimensional_winning_pattern_list = [
@@ -22,93 +24,56 @@ def pattern_match_debug(y, x):
           [-1, -1, player, -1, -1],
           [0, player, player, player, 0],
           [-1, -1, player, -1, -1],
-          [-1, -1, 0, -1, -1]], 2, 2),
-
-        ([[-1, -1, -1, 0, -1],
-          [-1, -1, -1, player, -1],
-          [-1, -1, -1, player, -1],
-          [0, player, player, player, 0],
-          [-1, -1, -1, 0, -1]], 3, 3),
-
-        ([[-1, 0, -1, -1, -1],
-          [-1, player, - 1, -1, -1],
-          [-1, player, - 1, -1, -1],
-          [0, player, player, player, 0],
-          [-1, 0, -1, -1, -1]], 3, 1),
-
-        ([[-1, -1, -1, 0, -1],
-          [0, player, player, player, 0],
-          [-1, -1, -1, player, -1],
-          [-1, -1, -1, player, -1],
-          [-1, -1, -1, 0, -1]], 1, 3),
+          [-1, -1, 0, -1, -1]], False, (2, 2)),
 
         ([[-1, 0, -1, -1, -1],
           [0, player, player, player, 0],
           [-1, player, - 1, -1, -1],
           [-1, player, - 1, -1, -1],
-          [-1, 0, -1, -1, -1]], 1, 1)
+          [-1, 0, -1, -1, -1]], True, (1, 1))
     ]
 
-    two_dimensional_important_pattern_list = [
+    two_dimensional_dangerous_pattern_list = [
         ([[-1, -1, 0, -1, -1],
           [-1, -1, opponent, -1, -1],
           [0, opponent, player, opponent, 0],
           [-1, -1, opponent, -1, -1],
-          [-1, -1, 0, -1, -1]], 2, 2),
-
-        ([[-1, -1, -1, 0, -1],
-          [-1, -1, -1, opponent, -1],
-          [-1, -1, -1, opponent, -1],
-          [0, opponent, opponent, player, 0],
-          [-1, -1, -1, 0, -1]], 3, 3),
-
-        ([[-1, 0, -1, -1, -1],
-          [-1, opponent, - 1, -1, -1],
-          [-1, opponent, - 1, -1, -1],
-          [0, player, opponent, opponent, 0],
-          [-1, 0, -1, -1, -1]], 3, 1),
-
-        ([[-1, -1, -1, 0, -1],
-          [0, opponent, opponent, player, 0],
-          [-1, -1, -1, opponent, -1],
-          [-1, -1, -1, opponent, -1],
-          [-1, -1, -1, 0, -1]], 1, 3),
+          [-1, -1, 0, -1, -1]], False, (2, 2)),
 
         ([[-1, 0, -1, -1, -1],
           [0, player, opponent, opponent, 0],
           [-1, opponent, - 1, -1, -1],
           [-1, opponent, - 1, -1, -1],
-          [-1, 0, -1, -1, -1]], 1, 1)
+          [-1, 0, -1, -1, -1]], True, (1, 1))
     ]
 
     one_dimensional_pattern_list = [
-        ([0, player, player, player, 0], 50, 3),
-        ([0, player, player, 0], 40, 2),
-        ([0, player, 0, player, 0], 30, 3),
-        ([opponent, player, player, player, player, 0], 20, 4),
-        ([0, player, player, player, player, opponent], 20, 4),
-        ([opponent, player, player, player, 0], 10, 3),
-        ([0, player, player, player, opponent], 10, 3),
-        ([opponent, player, player, 0], 1, 2),
-        ([0, player, player, opponent], 1, 2)
+        ([0, player, player, player, 0], False, 3, 50),
+        ([0, player, player, 0], False, 2, 40),
+        ([0, player, 0, player, 0], False, 3, 30),
+        ([opponent, player, player, player, player, 0], True, 4, 20),
+        ([opponent, player, player, player, 0], True, 3, 10),
+        ([opponent, player, player, 0], True, 2, 5),
+        ([opponent, player, 0], True, 1, 1)
     ]
 
     for pattern, left_offset in one_dimensional_winning_pattern_list:
-        if ai.evaluate.is_one_dimensional_pattern_match(pattern, x, y, left_offset):
-            print(pattern, ai.evaluate.is_one_dimensional_pattern_match(pattern, x, y, left_offset))
+        if ai.evaluate.one_dimensional_pattern_match(pattern, False, x, y, left_offset):
+            print(pattern, ai.evaluate.one_dimensional_pattern_match(pattern, False, x, y, left_offset))
 
-    for pattern, left_offset, right_offset in one_dimensional_important_pattern_list:
-        if ai.evaluate.is_one_dimensional_pattern_match(pattern, x, y, left_offset, right_offset):
-            print(pattern, ai.evaluate.is_one_dimensional_pattern_match(pattern, x, y, left_offset, right_offset))
+    for pattern, need_reverse, left_offset, right_offset in one_dimensional_dangerous_pattern_list:
+        if ai.evaluate.one_dimensional_pattern_match(pattern, need_reverse, x, y, left_offset, right_offset):
+            print(pattern,
+                  ai.evaluate.one_dimensional_pattern_match(pattern, need_reverse, x, y, left_offset, right_offset))
 
-    for pattern, anchor_x, anchor_y in two_dimensional_winning_pattern_list:
-        if ai.evaluate.is_two_dimensional_pattern_match(pattern, x, y, anchor_x, anchor_y) > 0:
-            print(pattern, ai.evaluate.is_two_dimensional_pattern_match(pattern, x, y, anchor_x, anchor_y))
+    for pattern, need_rotate, (anchor_x, anchor_y) in two_dimensional_winning_pattern_list:
+        if ai.evaluate.two_dimensional_pattern_match(pattern, need_rotate, x, y, anchor_x, anchor_y):
+            print(numpy.array(pattern), ai.evaluate.two_dimensional_pattern_match(pattern, need_rotate, x, y, anchor_x, anchor_y))
 
-    for pattern, anchor_x, anchor_y in two_dimensional_important_pattern_list:
-        if ai.evaluate.is_two_dimensional_pattern_match(pattern, x, y, anchor_x, anchor_y):
-            print(pattern, ai.evaluate.is_two_dimensional_pattern_match(pattern, x, y, anchor_x, anchor_y))
+    for pattern, need_rotate, (anchor_x, anchor_y) in two_dimensional_dangerous_pattern_list:
+        if ai.evaluate.two_dimensional_pattern_match(pattern, need_rotate, x, y, anchor_x, anchor_y):
+            print(numpy.array(pattern), ai.evaluate.two_dimensional_pattern_match(pattern, need_rotate, x, y, anchor_x, anchor_y))
 
-    for pattern, expect_reward, left_offset in one_dimensional_pattern_list:
-        if ai.evaluate.is_one_dimensional_pattern_match(pattern, x, y, left_offset):
-            print(pattern, ai.evaluate.is_one_dimensional_pattern_match(pattern, x, y, left_offset))
+    for pattern, need_reverse, left_offset, _ in one_dimensional_pattern_list:
+        if ai.evaluate.one_dimensional_pattern_match(pattern, need_reverse, x, y, left_offset):
+            print(pattern, ai.evaluate.one_dimensional_pattern_match(pattern, need_reverse, x, y, left_offset))
--- ai/evaluate.py
+++ ai/evaluate.py
@@ -19,9 +19,9 @@ def evaluation(x: int, y: int) -> int:
         ([0, player, player, player, player, 0], 4),
     ]
 
-    one_dimensional_important_pattern_list = [
+    one_dimensional_dangerous_pattern_list = [
         ([0, opponent, opponent, opponent, player], 4, 1),
-        ([player, opponent, opponent, opponent, 0], 0, 5)
+        ([opponent, opponent, opponent, opponent, player], 4, 1)
     ]
 
     two_dimensional_winning_pattern_list = [
@@ -29,71 +29,57 @@ def evaluation(x: int, y: int) -> int:
           [-1, -1, player, -1, -1],
           [0, player, player, player, 0],
           [-1, -1, player, -1, -1],
-          [-1, -1, 0, -1, -1]], 2, 2),
-
-        ([[-1, -1, -1, 0, -1],
-          [-1, -1, -1, player, -1],
-          [-1, -1, -1, player, -1],
-          [0, player, player, player, 0],
-          [-1, -1, -1, 0, -1]], 3, 3),
+          [-1, -1, 0, -1, -1]], False, (2, 2)),
 
         ([[-1, 0, -1, -1, -1],
           [0, player, player, player, 0],
           [-1, player, - 1, -1, -1],
           [-1, player, - 1, -1, -1],
-          [-1, 0, -1, -1, -1]], 1, 1)
+          [-1, 0, -1, -1, -1]], True, (1, 1))
     ]
 
-    two_dimensional_important_pattern_list = [
+    two_dimensional_dangerous_pattern_list = [
         ([[-1, -1, 0, -1, -1],
           [-1, -1, opponent, -1, -1],
           [0, opponent, player, opponent, 0],
           [-1, -1, opponent, -1, -1],
-          [-1, -1, 0, -1, -1]], 2, 2),
-
-        ([[-1, -1, -1, 0, -1],
-          [-1, -1, -1, opponent, -1],
-          [-1, -1, -1, opponent, -1],
-          [0, opponent, opponent, player, 0],
-          [-1, -1, -1, 0, -1]], 3, 3),
+          [-1, -1, 0, -1, -1]], False, (2, 2)),
 
         ([[-1, 0, -1, -1, -1],
           [0, player, opponent, opponent, 0],
           [-1, opponent, - 1, -1, -1],
           [-1, opponent, - 1, -1, -1],
-          [-1, 0, -1, -1, -1]], 1, 1)
+          [-1, 0, -1, -1, -1]], True, (1, 1))
     ]
 
     one_dimensional_pattern_list = [
-        ([0, player, player, player, 0], 50, 3),
-        ([0, player, player, 0], 40, 2),
-        ([0, player, 0, player, 0], 30, 3),
-        ([opponent, player, player, player, player, 0], 20, 4),
-        ([0, player, player, player, player, opponent], 20, 4),
-        ([opponent, player, player, player, 0], 10, 3),
-        ([0, player, player, player, opponent], 10, 3),
-        ([opponent, player, player, 0], 1, 2),
-        ([0, player, player, opponent], 1, 2)
+        ([0, player, player, player, 0], False, 3, 50),
+        ([0, player, player, 0], False, 2, 40),
+        ([0, player, 0, player, 0], False, 3, 30),
+        ([opponent, player, player, player, player, 0], True, 4, 20),
+        ([opponent, player, player, player, 0], True, 3, 10),
+        ([opponent, player, player, 0], True, 2, 5),
+        ([opponent, player, 0], True, 1, 1)
     ]
 
     for pattern, left_offset in one_dimensional_winning_pattern_list:
-        if is_one_dimensional_pattern_match(pattern, x, y, left_offset):
+        if one_dimensional_pattern_match(pattern, False, x, y, left_offset):
             return 100
 
-    for pattern, left_offset, right_offset in one_dimensional_important_pattern_list:
-        if is_one_dimensional_pattern_match(pattern, x, y, left_offset, right_offset):
+    for pattern, left_offset, right_offset in one_dimensional_dangerous_pattern_list:
+        if one_dimensional_pattern_match(pattern, True, x, y, left_offset, right_offset):
             return 100
 
-    for pattern, anchor_x, anchor_y in two_dimensional_winning_pattern_list:
-        if is_two_dimensional_pattern_match(pattern, x, y, anchor_x, anchor_y):
+    for pattern, need_rotate, (anchor_x, anchor_y) in two_dimensional_winning_pattern_list:
+        if two_dimensional_pattern_match(pattern, need_rotate, x, y, anchor_x, anchor_y):
             return 100
 
-    for pattern, anchor_x, anchor_y in two_dimensional_important_pattern_list:
-        if is_two_dimensional_pattern_match(pattern, x, y, anchor_x, anchor_y):
+    for pattern, need_rotate, (anchor_x, anchor_y) in two_dimensional_dangerous_pattern_list:
+        if two_dimensional_pattern_match(pattern, need_rotate, x, y, anchor_x, anchor_y):
             return 100
 
-    for pattern, expect_reward, left_offset in one_dimensional_pattern_list:
-        reward += expect_reward * is_one_dimensional_pattern_match(pattern, x, y, left_offset)
+    for pattern, need_reverse, left_offset, expect_reward in one_dimensional_pattern_list:
+        reward += expect_reward * one_dimensional_pattern_match(pattern, need_reverse, x, y, left_offset)
 
     return reward
 
@@ -101,18 +87,14 @@ def evaluation(x: int, y: int) -> int:
 def has_winner(pattern: [], x, y) -> bool:
     global chess
     chess = ai.chess
-    return is_one_dimensional_pattern_match(pattern, x, y, 4) > 0
+    return one_dimensional_pattern_match(pattern, False, x, y, 4) > 0
 
 
-def is_one_dimensional_pattern_match(pattern: [], x: int, y: int, l_ofs: int, *r_offset: int) -> int:
+def get_1d_reward(pattern: [], x: int, y: int, l_ofs: int, r_ofs: int) -> int:
     diff = y - x
     rot = x - 22 + y
     bias = 0 if diff <= 0 else abs(diff)
     rot_bias = 0 if rot <= 0 else abs(rot)
-    if len(r_offset) == 0:
-        r_ofs = l_ofs + 1
-    else:
-        r_ofs = r_offset[0]
 
     return (int(is_pattern_match([pattern], [np.array(ai.chess)[x, y - l_ofs: y + r_ofs]])) +
             int(is_pattern_match([pattern], [np.array(ai.chess)[x - l_ofs: x + r_ofs, y]])) +
@@ -121,7 +103,18 @@ def is_one_dimensional_pattern_match(pattern: [], x: int, y: int, l_ofs: int, *r
                                              x - rot_bias - l_ofs:x - rot_bias + r_ofs]])))
 
 
-def is_two_dimensional_pattern_match(pattern: [[]], x: int, y: int, anchor_x: int, anchor_y: int) -> int:
+def one_dimensional_pattern_match(pattern: [], need_reverse: bool, x: int, y: int, l_ofs: int, *r_offset: int) -> int:
+    if len(r_offset) == 0:
+        r_ofs = l_ofs + 1
+    else:
+        r_ofs = r_offset[0]
+    reward = get_1d_reward(pattern, x, y, l_ofs, r_ofs)
+    if need_reverse:
+        reward += get_1d_reward(list(reversed(pattern)), x, y, r_ofs - 1, l_ofs + 1)
+    return reward
+
+
+def get_2d_reward(pattern: [[]], x: int, y: int, anchor_x: int, anchor_y: int) -> int:
     diff = y - x
     u_ofs = anchor_y
     d_ofs = len(pattern) - anchor_y
@@ -136,17 +129,25 @@ def is_two_dimensional_pattern_match(pattern: [[]], x: int, y: int, anchor_x: in
         offset = diff + (i - y) * 2
         bias = 0 if offset <= 0 else offset
         diagonal.append(np.diagonal(ai.chess, offset=offset)[i - bias - l_ofs:i - bias + r_ofs])
-
     anti_diagonal = []
     for i in range(x - u_ofs, x + d_ofs):
         offset = rot + (i - x) * 2
         rot_bias = 0 if offset <= 0 else offset
         anti_diagonal.append(np.diagonal(np.rot90(ai.chess), offset=offset)[i - rot_bias - l_ofs:i - rot_bias + r_ofs])
 
-    return (
-        int(is_pattern_match(pattern, np.array(ai.chess)[x - u_ofs: x + d_ofs, y - l_ofs: y + r_ofs])) +
-        int(is_pattern_match(pattern, np.rot90(ai.chess)[rot_x - u_ofs: rot_x + d_ofs, rot_y - l_ofs: rot_y + r_ofs])) +
-        int(is_pattern_match(pattern, diagonal)) + int(is_pattern_match(pattern, anti_diagonal)))
+    return (int(is_pattern_match(pattern, np.array(ai.chess)[x - u_ofs: x + d_ofs, y - l_ofs: y + r_ofs])) +
+            int(is_pattern_match(pattern,
+                                 np.rot90(ai.chess)[rot_x - u_ofs: rot_x + d_ofs, rot_y - l_ofs: rot_y + r_ofs])) +
+            int(is_pattern_match(pattern, diagonal)) + int(is_pattern_match(pattern, anti_diagonal)))
+
+
+def two_dimensional_pattern_match(pattern: [[]], need_rotate: bool, x: int, y: int,
+                                  anchor_x: int, anchor_y: int) -> int:
+    reward = get_2d_reward(pattern, x, y, anchor_x, anchor_y)
+    if need_rotate:
+        reward += get_2d_reward(np.rot90(np.rot90(pattern)), x, y, len(pattern) - anchor_x - 1,
+                                len(pattern[0]) - anchor_y - 1)
+    return reward
 
 
 def is_pattern_match(pattern: [[]], array: [[]]) -> bool:
