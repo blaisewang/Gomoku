6831b17a9f1f5704109df8e6e67fcedf72d8b711
blaisewang <kaito.kaito.k@gmail.com>
2018-04-27 01:14:24 +0100

    minor improve

--- human_play.py
+++ human_play.py
@@ -1,13 +1,17 @@
+import sys
+
 import wx
 
 import ui
 
 
-def run():
+def run(n: int):
     app = wx.App(False)
-    ui.GomokuFrame(n=11)
+    ui.GomokuFrame(n=n)
     app.MainLoop()
 
 
 if __name__ == '__main__':
-    run()
+    length = sys.argv[1]
+    if length.isdigit():
+        run(int(length))
